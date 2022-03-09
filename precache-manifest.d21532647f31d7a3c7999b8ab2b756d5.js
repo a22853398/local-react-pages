c8f57785d14b879b11fca26cc19f1cfbdd68c9d9
weihung_lai <u9908028@gmail.com>
2022-03-09 12:14:10 +0800

    Updates

---
+++ precache-manifest.d21532647f31d7a3c7999b8ab2b756d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8504a20dae1187aa9c9",
+    "url": "/local-react-pages/static/css/main.c4f3087b.chunk.css"
+  },
+  {
+    "revision": "a8504a20dae1187aa9c9",
+    "url": "/local-react-pages/static/js/main.a8504a20.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/local-react-pages/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "c23bf023f8898208c880",
+    "url": "/local-react-pages/static/js/2.c23bf023.chunk.js"
+  },
+  {
+    "revision": "6adaea00e436081bd45d",
+    "url": "/local-react-pages/static/js/runtime~main.6adaea00.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/local-react-pages/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "d158f4dc8f987f4d7e9c32d52ca37b53",
+    "url": "/local-react-pages/index.html"
+  }
+];
